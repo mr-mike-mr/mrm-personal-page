2f75f04c320d0b00efae6dd977e8846d0cc1eabd
mike <mr_mike_eu@proton.me>
2025-02-04 17:34:47 +0100

    add comments into projects + remove duplicity

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,21 +5,21 @@
     import reminder_of_important_dates from "$lib/assets/projects/reminder_of_important_dates.png";
 </script>
 
+<!-- PROJECTS ROOT -->
 <div class="projects_root">
+    <!-- PROJECT - Simple Chat App -->
     <div class="project">
+        <!-- IMAGE AND HEADING -->
         <div>
+            <!-- IMAGE -->
             <img alt="Project screenshot" src={simple_chat_project} />
+            <!-- HEADING -->
             <h1>Simple Chat App</h1>
         </div>
+        <!-- LINK -->
         <a target="_blank" href="https://github.com/mr-mike-mr/simple-chat-app">SHOW MORE</a>
     </div>
-    <div class="project">
-        <div>
-            <img alt="Project screenshot" src={reminder_of_important_dates} />
-            <h1>Reminder of Important dates</h1>
-        </div>
-        <a target="_blank" href="https://github.com/mr-mike-mr/mrm-reminder-of-important-dates?tab=readme-ov-file">SHOW MORE</a>
-    </div>
+    <!-- PROJECT - Reminder of Important dates -->
     <div class="project">
         <div>
             <img alt="Project screenshot" src={reminder_of_important_dates} />
