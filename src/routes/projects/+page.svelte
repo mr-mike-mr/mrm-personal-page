18350c8191d3af50be88407afed04c7fe4c5cc9c
mike <mr_mike_eu@proton.me>
2025-02-04 17:28:01 +0100

    remove .svg files, remove icons folder, add svg inline, add style and rwd and theme for projects, add project folder into assets, add new blog_banner_example.png, add background for footer

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,37 +1,30 @@
 <!-- SCRIPT -->
 <script>
-	// import assets
-	import arrow_left_circle_icon from "$lib/assets/icons/arrow_left_circle.svg";
-	import arrow_right_circle_icon from "$lib/assets/icons/arrow_right_circle.svg";
-
-    // Past project function
-    function past_project() {
-        // TODO:
-    }
-
-    // Next project function
-    function next_project() {
-        // TODO:
-    }
+    // import assets
+	import simple_chat_project from "$lib/assets/projects/simple_chat_project.png";
+    import reminder_of_important_dates from "$lib/assets/projects/reminder_of_important_dates.png";
 </script>
 
 <div class="projects_root">
-    <button on:click={past_project}><img alt="Arrow left circle icon" src={arrow_left_circle_icon} /></button>
-
-    <div class="project_frame">
-        <h1>Project name</h1>
-        <p>Project description</p>
-        <div class="project_tags">
-            <div>
-                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>HTML5</title><path style="fill: #E44D26;" d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/></svg>
-                <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</a>
-            </div>
-            <div>
-                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>CSS</title><path style="fill: #663399;" d="M0 0v20.16A3.84 3.84 0 0 0 3.84 24h16.32A3.84 3.84 0 0 0 24 20.16V3.84A3.84 3.84 0 0 0 20.16 0Zm14.256 13.08c1.56 0 2.28 1.08 2.304 2.64h-1.608c.024-.288-.048-.6-.144-.84-.096-.192-.288-.264-.552-.264-.456 0-.696.264-.696.84-.024.576.288.888.768 1.08.72.288 1.608.744 1.92 1.296q.432.648.432 1.656c0 1.608-.912 2.592-2.496 2.592-1.656 0-2.4-1.032-2.424-2.688h1.68c0 .792.264 1.176.792 1.176.264 0 .456-.072.552-.24.192-.312.24-1.176-.048-1.512-.312-.408-.912-.6-1.32-.816q-.828-.396-1.224-.936c-.24-.36-.36-.888-.36-1.536 0-1.44.936-2.472 2.424-2.448m5.4 0c1.584 0 2.304 1.08 2.328 2.64h-1.608c0-.288-.048-.6-.168-.84-.096-.192-.264-.264-.528-.264-.48 0-.72.264-.72.84s.288.888.792 1.08c.696.288 1.608.744 1.92 1.296.264.432.408.984.408 1.656.024 1.608-.888 2.592-2.472 2.592-1.68 0-2.424-1.056-2.448-2.688h1.68c0 .744.264 1.176.792 1.176.264 0 .456-.072.552-.24.216-.312.264-1.176-.048-1.512-.288-.408-.888-.6-1.32-.816-.552-.264-.96-.576-1.2-.936s-.36-.888-.36-1.536c-.024-1.44.912-2.472 2.4-2.448m-11.031.018c.711-.006 1.419.198 1.839.63.432.432.672 1.128.648 1.992H9.336c.024-.456-.096-.792-.432-.96-.312-.144-.768-.048-.888.24-.12.264-.192.576-.168.864v3.504c0 .744.264 1.128.768 1.128a.65.65 0 0 0 .552-.264c.168-.24.192-.552.168-.84h1.776c.096 1.632-.984 2.712-2.568 2.688-1.536 0-2.496-.864-2.472-2.472v-4.032c0-.816.24-1.44.696-1.848.432-.408 1.146-.624 1.857-.63"/></svg>
-                <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</a>
-            </div>
+    <div class="project">
+        <div>
+            <img alt="Project screenshot" src={simple_chat_project} />
+            <h1>Simple Chat App</h1>
         </div>
+        <a target="_blank" href="https://github.com/mr-mike-mr/simple-chat-app">SHOW MORE</a>
     </div>
-
-    <button on:click={next_project}><img alt="Arrow right circle icon" src={arrow_right_circle_icon} /></button>
+    <div class="project">
+        <div>
+            <img alt="Project screenshot" src={reminder_of_important_dates} />
+            <h1>Reminder of Important dates</h1>
+        </div>
+        <a target="_blank" href="https://github.com/mr-mike-mr/mrm-reminder-of-important-dates?tab=readme-ov-file">SHOW MORE</a>
+    </div>
+    <div class="project">
+        <div>
+            <img alt="Project screenshot" src={reminder_of_important_dates} />
+            <h1>Reminder of Important dates</h1>
+        </div>
+        <a target="_blank" href="https://github.com/mr-mike-mr/mrm-reminder-of-important-dates?tab=readme-ov-file">SHOW MORE</a>
+    </div>
 </div>
