2ab19a7388d9fa035caf3669c1d0df43c697cc1a
mike <mr_mike_eu@proton.me>
2025-02-08 15:24:34 +0100

    add sql tables into README.md, add .env.example for database connect, add mysql2 and dotenv packages, move projects assets from lib to static, add projects loading from databse, fix padding in styles for nav bar space, add border for nav bar

--- README.md
+++ README.md
@@ -1,38 +1,25 @@
-# sv
+# MRM Personal page
 
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
+## SQL
 
-## Creating a project
+```sql
+CREATE TABLE projects
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    image VARCHAR(250) NOT NULL,
+    heading VARCHAR(250) NOT NULL,
+    links VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+);
 
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
+CREATE TABLE blog
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    thumbnail VARCHAR(250) NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    head VARCHAR(250) NOT NULL,
+    body VARCHAR(250) NOT NULL,
+    foot VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+);
 ```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,34 +1,27 @@
 <!-- SCRIPT -->
 <script>
-    // import assets
-	import simple_chat_project from "$lib/assets/projects/simple_chat_project.png";
-    import reminder_of_important_dates from "$lib/assets/projects/reminder_of_important_dates.png";
 	// import libs
 	import locales from "$lib/locales.json";
 	// import stores
 	import { lang } from "$lib/stores/user_store.js";
+    // load data from +page.server.js
+    /** @type {import('./$types').PageProps} */
+	let { data } = $props();
 </script>
 
 <!-- PROJECTS ROOT -->
 <div class="projects_root">
-    <!-- PROJECT - Simple Chat App -->
-    <div class="project">
-        <!-- IMAGE AND HEADING -->
-        <div>
-            <!-- IMAGE -->
-            <img alt="Project screenshot" src={simple_chat_project} />
-            <!-- HEADING -->
-            <h1>Simple Chat App</h1>
+    {#each data.projects as project}
+        <div class="project">
+            <!-- IMAGE AND HEADING -->
+            <div>
+                <!-- IMAGE -->
+                <img alt="Project screenshot" src={"/projects/"+project.image} />
+                <!-- HEADING -->
+                <h1>{project.heading}</h1>
+            </div>
+            <!-- LINK -->
+            <a target="_blank" href={project.links}>{locales[$lang]["13"]}</a>
         </div>
-        <!-- LINK -->
-        <a target="_blank" href="https://github.com/mr-mike-mr/simple-chat-app">{locales[$lang]["13"]}</a>
-    </div>
-    <!-- PROJECT - Reminder of Important dates -->
-    <div class="project">
-        <div>
-            <img alt="Project screenshot" src={reminder_of_important_dates} />
-            <h1>Reminder of Important dates</h1>
-        </div>
-        <a target="_blank" href="https://github.com/mr-mike-mr/mrm-reminder-of-important-dates?tab=readme-ov-file">{locales[$lang]["13"]}</a>
-    </div>
+    {/each}
 </div>
